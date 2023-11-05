8c7ac76318fc8e746339151cc3cdc5bdaff1af42
HIMANSHU SINGH <76735008+ITZMEHIMANSHUKING@users.noreply.github.com>
2023-11-05 13:26:53 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,13 +20,7 @@ package_folder_path = PACKAGE_NAME.replace('-', '/')
 # a-b-c => a.b.c
 namespace_name = PACKAGE_NAME.replace('-', '.')
 
-# Version extraction inspired from 'requests'
-with open(os.path.join(package_folder_path, 'version.py')
-          if os.path.exists(os.path.join(package_folder_path, 'version.py'))
-          else os.path.join(package_folder_path, '_version.py'), 'r') as fd:
-    version = re.search(r'^VERSION\s*=\s*[\'"]([^\'"]*)[\'"]',
-                        fd.read(), re.MULTILINE).group(1)
-
+version="0.1.0",
 if not version:
     raise RuntimeError('Cannot find version information')
 
